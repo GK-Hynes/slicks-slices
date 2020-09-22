cf3dcac5bda0bc8a27920a0f33d3e4e93f32a039
Gerard Hynes <gkhynes@gmail.com>
2020-09-22 22:28:18 +0100

    Don't Git init in the Gatsby directory

---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+function HomePage() {
+  return (
+    <div>
+      <Nav />
+      <p>Hey, I'm the home page</p>
+    </div>
+  );
+}
+
+export default HomePage;
