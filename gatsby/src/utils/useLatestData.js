a760829a5cc4984bacbfe8918ddbbd3d0c3f8a95
Gerard Hynes <gkhynes@gmail.com>
2020-10-31 17:09:50 +0000

    Add custom hook to fetch latest StoreSettings

---
+++ gatsby/src/utils/useLatestData.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+export default function useLatestData() {
+  // hot slices
+  const [hotSlices, setHotSlices] = useState();
+  //slicemasters
+  const [slicemasters, setSlicemasters] = useState();
+  // Use a side effect to fetch data from graphql endpoint
+  useEffect(function () {
+    // When the component loads, fetch the data
+    fetch(process.env.GATSBY_GRAPHQL_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+  StoreSettings(id: "downtown") {
+    name
+    slicemaster {
+      name
+    }
+    hotSlices {
+      name
+    }
+  }
+}
+        `
+      })
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        // TODO Check for errors
+        // Set the data to state
+        console.log(res.data);
+        setHotSlices(res.data.StoreSettings.hotSlices);
+        setSlicemasters(res.data.StoreSettings.slicemasters);
+      });
+  }, []);
+  return {
+    hotSlices,
+    slicemasters
+  };
+}
